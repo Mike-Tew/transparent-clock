dce63e56b276d31351b657843b37dc2f963eb4e6
Michael Tew <mike.tew@gmail.com>
2021-03-12 01:44:25 -0800

    Modifying opacity button states.

--- main.py
+++ main.py
@@ -1,44 +1,86 @@
 from tkinter import *
+import time
+x = time.time()
+y = time.time()
+print(x, y)
 
 
-def increase_opacity():
+running = False
+
+
+def start_timer():
+    """Start and display the timer."""
+    print(time.time())
+
+
+def pause_timer():
+    """Pause and display the timer."""
+    pass
+
+
+def reset_timer():
+    """Reset and display the timer."""
+    pass
+
+
+def increase_opacity(increase_opacity_button):
     """Increase the opacity of the app."""
 
+    decrease_opacity_button["state"] = "normal"
     opacity = root.attributes()[1]
-    root.attributes('-alpha', opacity + 0.1)
+
+    if opacity >= 1:
+        increase_opacity_button["state"] = "disabled"
+    else:
+        root.attributes("-alpha", opacity + 0.1)
 
 
-def decrease_opacity():
+def decrease_opacity(decrease_opacity_button):
     """Decrease the opacity of the app."""
 
+    increase_opacity_button["state"] = "normal"
     opacity = root.attributes()[1]
+
     if opacity <= 0.2:
-        root.attributes('-alpha', 0.1)
+        root.attributes("-alpha", 0.1)
+        decrease_opacity_button["state"] = "disabled"
     else:
-        root.attributes('-alpha', opacity - 0.1)
+        root.attributes("-alpha", opacity - 0.1)
 
 
 root = Tk()
-root.geometry("200x150+800+300")
+root.geometry("300x150+800+300")
 
 timer_frame = LabelFrame(root)
 timer_frame.grid(row=0, column=0)
 
-timer_label = Label(timer_frame, text="Timer", font=("Helvetica 20"))
-timer_label.grid(row=0, column=0)
+timer_label = Label(timer_frame, text="00:00:00", font=("Helvetica 20"))
+timer_label.grid(row=0, column=0, columnspan=3)
 
-start_button = Button(timer_frame, text="")
+start_button = Button(timer_frame, text="Start", command=start_timer)
+start_button.grid(row=1, column=0)
 
-opacity_frame = LabelFrame(root)
-opacity_frame.grid(row=0, column=1)
+pause_button = Button(timer_frame, text="Pause", command=pause_timer)
+pause_button.grid(row=1, column=1)
 
-opacity_up_button = Button(opacity_frame, text="+", width=2, font=("Helvetica 15"), command=increase_opacity)
-opacity_up_button.grid(row=0, column=3)
+reset_button = Button(timer_frame, text="Reset", command=reset_timer)
+reset_button.grid(row=1, column=2)
 
-opacity_down_button = Button(opacity_frame, text="-", width=2, font=("Helvetica 15"), command=decrease_opacity)
-opacity_down_button.grid(row=1, column=3)
 
-root.attributes('-alpha', 0.9)
+opacity_frame = LabelFrame(root, text="Opacity")
+opacity_frame.grid(row=0, column=1, padx=[20, 0])
+
+increase_opacity_button = Button(
+    opacity_frame, text="+", width=2, font=("Helvetica 15"), command=lambda:increase_opacity(increase_opacity_button)
+)
+increase_opacity_button.grid(row=0, column=3)
+
+decrease_opacity_button = Button(
+    opacity_frame, text="-", width=2, font=("Helvetica 15"), command=lambda:decrease_opacity(decrease_opacity_button)
+)
+decrease_opacity_button.grid(row=1, column=3)
+
+root.attributes("-alpha", 0.9)
 # root.attributes('-topmost', True)
 # root.update()
 
