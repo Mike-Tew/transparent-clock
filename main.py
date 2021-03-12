0b1308baaf401bfb62ef189287172a66594450f0
Michael Tew <mike.tew@gmail.com>
2021-03-12 01:58:12 -0800

    Experimenting with the stopwatch.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from tkinter import *
+from tkinter import Tk, LabelFrame, Label, Button
+from datetime import datetime
 import time
-x = time.time()
-y = time.time()
-print(x, y)
 
-
+counter = 0
 running = False
 
+def counting():
+    global counter
+    tt = datetime.fromtimestamp(100)
+    string = tt.strftime("%H:%M:%S")
+    display = string
 
-def start_timer():
+
+
+
+def start_timer(timer_label):
     """Start and display the timer."""
     print(time.time())
+    # timer_label.after(1000, )
+    timer_label['text'] = string
 
 
 def pause_timer():
@@ -57,7 +65,7 @@ timer_frame.grid(row=0, column=0)
 timer_label = Label(timer_frame, text="00:00:00", font=("Helvetica 20"))
 timer_label.grid(row=0, column=0, columnspan=3)
 
-start_button = Button(timer_frame, text="Start", command=start_timer)
+start_button = Button(timer_frame, text="Start", command=lambda: start_timer(timer_label))
 start_button.grid(row=1, column=0)
 
 pause_button = Button(timer_frame, text="Pause", command=pause_timer)
@@ -71,12 +79,20 @@ opacity_frame = LabelFrame(root, text="Opacity")
 opacity_frame.grid(row=0, column=1, padx=[20, 0])
 
 increase_opacity_button = Button(
-    opacity_frame, text="+", width=2, font=("Helvetica 15"), command=lambda:increase_opacity(increase_opacity_button)
+    opacity_frame,
+    text="+",
+    width=2,
+    font=("Helvetica 15"),
+    command=lambda: increase_opacity(increase_opacity_button),
 )
 increase_opacity_button.grid(row=0, column=3)
 
 decrease_opacity_button = Button(
-    opacity_frame, text="-", width=2, font=("Helvetica 15"), command=lambda:decrease_opacity(decrease_opacity_button)
+    opacity_frame,
+    text="-",
+    width=2,
+    font=("Helvetica 15"),
+    command=lambda: decrease_opacity(decrease_opacity_button),
 )
 decrease_opacity_button.grid(row=1, column=3)
 
